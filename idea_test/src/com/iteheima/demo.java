8fa5ac7f4e71332b915e629481f17f09c3a1104c
zg <3114227397@qq.com>
2022-08-11 13:47:50 +0800

    成绩的提升百分数

--- idea_test/src/com/iteheima/demo.java
+++ idea_test/src/com/iteheima/demo.java
@@ -3,16 +3,27 @@ package com.iteheima;//package com.itheima.web;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.Vector;
 
 public class demo {
     public static void main(String[] args) throws UnsupportedEncodingException {
-        String name="张三";
-        String encode = URLEncoder.encode(name, "UTF-8");
-        System.out.println(encode);
-        String decode = URLDecoder.decode(name, "utf-8");
-        System.out.println(decode);
-        byte[] bytes=decode.getBytes("ISO_8859_1");
-        String s=new String(bytes,"utf-8");
-        System.out.println(s);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("please input your old score:");
+        double old_score=sc.nextDouble();
+        System.out.println("please input your new score:");
+        double new_score=sc.nextDouble();
+        double ans=new_score/old_score;
+        DecimalFormat df=new DecimalFormat("0.00%");//格式化对象
+        System.out.println(df.format(ans));//format格式化方法
+
     }
+
+
+
+
 }
+
